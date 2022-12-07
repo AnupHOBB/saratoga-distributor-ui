088e9e27b04f4bf2d73ee9c9a448ba5a3840c154
Anup Bhandari <anup.b@HOBB.COM>
2022-12-07 17:56:00 +0530

    Fixed issue of prefetch getting halted on error and refactored code

--- loading-screen/loading-screen.js
+++ loading-screen/loading-screen.js
@@ -3,20 +3,20 @@ const { delay } = rxjs.operators
 
 const IMAGE_PREFIX = "loading-screen-icon/LoadingScreen"
 const IMAGE_SUFFIX = ".webp"
-const DELAY_IN_MILLIS = 500
-const MODELS = ["https://modelviewer.dev/assets/ShopifyModels/Chair.glb","https://modelviewer.dev/shared-assets/models/Astronaut.glb"]
+const DELAY_IN_MILLIS = 250
+const MODELS = ["https://modelviewer.dev/assets/ShopifyModels/Chair.glb", "https://modelviewer.dev/shared-assets/models/Astronaut.glb"]
 
 let loaderIcon
 let loaderText
 let head
-let percentage = 0
+let roundedPercentage = 0
 
 function onLoad()
 {
     loaderIcon = document.getElementById("loader-icon");
     loaderText = document.getElementById("loader-text");
     head = document.querySelector("head")
-    showLoaderText(1)
+    showLoaderText(0)
     new Observable((subscriber) => onSubscribe(subscriber, 0))
     .subscribe(
         {
@@ -37,12 +37,13 @@ function onSubscribe(subscriber, i)
         link.href = MODELS[i]
         link.as = "fetch"
         link.onload = ()=>{ onSubscribe(subscriber, i+1) }
+        link.onerror = ()=>{ onSubscribe(subscriber, i+1) }
         head.appendChild(link)
     }
     else
     {
         subscriber.next(MODELS.length)
-        setTimeout(() => subscriber.complete(), 1500)
+        setTimeout(() => subscriber.complete(), 1000)
     }
 }
 
@@ -50,32 +51,29 @@ function changeLoaderIcon(index)
 {
     if (index > 0)
     {
-        let roughPercentage = (index * 100)/MODELS.length
-        percentage = (roughPercentage<100)?(Math.floor(roughPercentage/5) * 5):100
-        loaderIcon.src = IMAGE_PREFIX+percentage+IMAGE_SUFFIX
+        let percentage = (index * 100)/MODELS.length
+        roundedPercentage = (percentage<100)?(Math.floor(percentage/5) * 5):100
+        loaderIcon.src = IMAGE_PREFIX+roundedPercentage+IMAGE_SUFFIX
     }
 }
 
 function showLoaderText(dots)
 {
-    dots = (dots > 3) ? 1 : dots
-    let dotText = ""
-    for(let i=0; i<dots; i++)
-        dotText += "."
-    dotText += " "
-    loaderText.innerHTML = "LOADING"+dotText+" "+percentage+"%"
-    setTimeout(() => showLoaderText(dots+1), DELAY_IN_MILLIS)
+    if (roundedPercentage < 100)
+    {
+        dots = (dots > 3) ? 0 : dots
+        let dotText = ""
+        for(let i=0; i<dots; i++)
+            dotText += "."
+        dotText += " "
+        loaderText.innerHTML = "LOADING"+dotText+" "+roundedPercentage+"%"
+        setTimeout(() => showLoaderText(dots+1), DELAY_IN_MILLIS)
+    }
+    else
+        loaderText.innerHTML = "LOADING... 100%"
 }
 
 function onLoadingComplete()
 {
-    let body = document.querySelector("body")
-    body.parentElement.removeChild(body)
-    
-    let html = document.querySelector("html")
-    let object = document.createElement("object")
-    object.style = "width: 100%; height: 100%;"
-    object.type = "text/html"
-    object.data = "loaded-screen.html"
-    html.appendChild(object)
+    window.location.replace("loaded-screen.html")
 }
